625c9c387af0052a5cb5b50722f693dda236abf3
ibaragozz <baragozz1@gmail.com>
2024-12-04 21:34:13 +0300

    Сделал модель главной страницы

--- flower_shop/bouquets/admin.py
+++ flower_shop/bouquets/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Bouquet
 
-admin.site.register(Bouquet)
+# Регистрация модели Bouquet в админке
+@admin.register(Bouquet)
+class BouquetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')  # Отображение полей в списке
+    search_fields = ('name',)  # Поле для поиска по названию
+    list_filter = ('price', 'created_at')  # Фильтры
--- flower_shop/bouquets/models.py
+++ flower_shop/bouquets/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Bouquet(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='bouquet_images/')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='bouquets/')
+    created_at = models.DateTimeField(auto_now_add=True)  # Поле для времени создания
 
     def __str__(self):
         return self.name
