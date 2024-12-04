686bd1449937c5b5b3d5e150dc8604ba43acd3dd
ibaragozz <baragozz1@gmail.com>
2024-12-04 21:18:16 +0300

    update project

--- flower_shop/bouquets/views.py
+++ flower_shop/bouquets/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Bouquet
 
-# Create your views here.
+def index(request):
+    bouquets = Bouquet.objects.all()
+    return render(request, 'index.html', {'bouquets': bouquets})
--- flower_shop/flower_shop/settings.py
+++ flower_shop/flower_shop/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bouquets'
+    'bouquets',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
