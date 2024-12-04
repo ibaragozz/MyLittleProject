b5d634c9f081254d96021e8593c927c5ce70cb48
ibaragozz <baragozz1@gmail.com>
2024-12-04 20:57:50 +0300

    Delaem zanogo po gpt

--- flower_shop/flower_shop/wsgi.py
+++ flower_shop/flower_shop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MLP project.
+WSGI config for flower_shop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MLP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flower_shop.settings')
 
 application = get_wsgi_application()
