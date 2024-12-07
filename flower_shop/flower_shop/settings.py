dc2c1f8cea2937764d2caa7d2e30883575ab6b74
ibaragozz <baragozz1@gmail.com>
2024-12-07 17:06:32 +0300

    fixing register

--- flower_shop/flower_shop/settings.py
+++ flower_shop/flower_shop/settings.py
@@ -133,3 +133,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'  # на главную страницу после входа
+LOGOUT_REDIRECT_URL = 'home'  # на главную страницу после выхода
--- flower_shop/users/views.py
+++ flower_shop/users/views.py
@@ -1,3 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт создан для {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # на главную страницу после входа
+        else:
+            messages.error(request, 'Неверное имя пользователя или пароль')
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')  # перенаправляем на главную после выхода
 from django.shortcuts import render
 
 # Create your views here.
