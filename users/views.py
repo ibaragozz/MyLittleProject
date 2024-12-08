db63d5c6fed2c59373eb3461fe0957c4eb0d20f3
ibaragozz <baragozz1@gmail.com>
2024-12-08 11:44:33 +0300

    edit base

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import RegisterForm
 
 def register(request):
@@ -9,22 +10,30 @@ def register(request):
         if form.is_valid():
             user = form.save()  # Сохраняем пользователя
             login(request, user)  # Авторизуем пользователя сразу после регистрации
-            return redirect('home')  # Перенаправление на главную страницу
+            messages.success(request, "Вы успешно зарегистрированы! Пожалуйста, войдите.")  # Сообщение об успешной регистрации
+            return redirect('home')  # Редирект на главную страницу после регистрации
     else:
-        form = RegisterForm()
+        form = RegisterForm()  # Если это GET-запрос, показываем пустую форму
 
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form})  # Рендерим страницу с формой регистрации
+
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
             login(request, user)
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'users/login.html', {'form': form})
+            return redirect('home')  # Редирект на главную страницу после входа
+
+        else:
+            # Логика для ошибки аутентификации
+            messages.error(request, "Неверный логин или пароль.")
+            return redirect('login')
+
+    return render(request, 'users/login.html')
 
 def logout_view(request):
     logout(request)
